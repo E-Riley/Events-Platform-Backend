878ab09be5706ff8aca4e2a756e8f192ac359fb5
ethan <ethanjriley02@gmail.com>
2025-06-01 16:25:15 +0100

    Adds cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const apiRouter = require("./routes/api.router");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use((req, res, next) => {
